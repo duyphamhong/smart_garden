342c1a068af249c852338d2b45b2019f6cd718c1
duyph <duy.phamhong@nashtechglobal.com>
2019-05-16 00:11:19 +0700

    a

--- Customer.Api/Controllers/ValuesController.cs
+++ Customer.Api/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace Customer.Api.Controllers
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
-        {
+        { 
             return new string[] { "value1", "value2" };
         }
 
